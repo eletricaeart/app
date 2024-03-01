2a81d911a5040ca39b0a7b97381b1155f4aeb072
@Ceo-Sammarco <noah.kd@gmail.com>
2024-02-29 21:30:07 -0300

    @Ceo-Sammarco - qui 29 fev 2024

--- client/src/utils/ceo.js
+++
@@ -1,9 +0,0 @@
-
-
-export default {
-   host: "http://127.0.0.1:",
-   gate: "7879",
-   serverGate: "8080",
-   corsGate: "8081",
-};
-
--- server/src/utils/ceo.js
+++
@@ -1,7 +0,0 @@
-
-
-exports.ceo = [
-	gate = "7879",
-	serverGate = "8080",
-	corsGate = "8081",
-];
