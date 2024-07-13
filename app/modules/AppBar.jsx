f0dbfd4807e2567dc99b44204f5e6a6371bdb980
Ceo-Sammarco <noah.kd@gmail.com>
2024-07-13 10:53:56 -0300

    @Ceo-Sammarco - Sat 13 Jul 24

--- app/modules/AppBar.jsx
+++ app/modules/AppBar.jsx
@@ -14,7 +14,40 @@ export default function AppBar( props ) {
 
    return( <>
       <View style={ style.appbar }>
-         <Text>AppBar</Text>
+         <View content>
+            <View appbar-menu-left>
+               <View content>
+                  <View back-btn id="backBtn">
+                     <View bar id="backBtn_topBar"></View>
+                     <View bar id="backBtn_centerBar"></View>
+                     <View bar id="backBtn_bottomBar"></View>
+                  </View>
+               </View>
+            </View>
+            <View appbar-logo>
+               <View ea-logo>
+                  <View logo-icon>
+                     <img src="./ea.jpg" alt="ea" />
+                  </View>
+                  <View logo-title>
+                     <View content>
+                        <Text tt>ELETRICA</Text>
+                        <Text tt>&</Text>
+                        <Text tt>ART</Text>
+                     </View>
+                  </View>
+               </View>
+            </View>
+            <View appbar-menu-right>
+               <View content id="">
+                  <View trigram id="openDrawer">
+                     <View bar id="topBar_left"></View>
+                     <View bar id="centerBar_left"></View>
+                     <View bar id="bottomBar_left"></View>
+                  </View>
+               </View>
+            </View>
+         </View>
       </View>
    </> );
 }
