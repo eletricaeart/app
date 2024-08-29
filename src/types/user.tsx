531c85d29538cb3945646021e82e6e388c569a13
Ceo-Sammarco <noah.kd@gmail.com>
2024-08-29 12:50:09 -0300

    Ceo-Sammarco - Thu 29 Aug 2024

--- src/types/user.tsx
+++ src/types/user.tsx
@@ -1,3 +1,5 @@
+import A from '../../../Dev/Js/Advanced/a';
+ />
 
 
 export interface User {
@@ -5,4 +7,39 @@ export interface User {
    name: string;
    email: string;
    role: "admin" | "user";
+}
+
+
+function updateUser( id: number, update: Partial<User> ) {
+   const 
+      user = getUser( id ),
+      newUser = { ...user, ...update }
+   ;
+   saveUser( id, newUser );
+}
+ 
+
+
+import * as React from "react";
+ 
+interface UserThumbnailProps {
+  img: string;
+  alt: string;
+  url: string;
+}
+ 
+export const UserThumbnail = ( props: UserThumbnailProps ) =>
+   <a href={ props.url }>
+      <img src={ props.img } alt={ props.alt }/>
+   </a>
+;
+
+
+interface Account {
+   id: number;
+   displayName: string;
+}
+ 
+function welcome( user: Account ) {
+   console.log( user.id );
 }
